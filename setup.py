c69e43102e6dd044e778b94c07e58b56f8502078
Tyson Smith <tyson.w.smith@gmail.com>
2020-05-05 09:58:46 -0700

    Update packaging

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """setuptools install script"""
-
+import pathlib
 from setuptools import setup
 
 if __name__ == "__main__":
+    README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
     setup(
         author="Tyson Smith",
+        author_email="twsmith@mozilla.com",
         description="PrefPicker - Manage & generate prefs.js files",
         entry_points={
             "console_scripts": ["prefpicker = prefpicker.main:main"]
@@ -19,14 +22,17 @@ if __name__ == "__main__":
             "pytest-cov",
             "pytest-pylint"
         ],
+        include_package_data=True,
         install_requires=[
             "pyyaml"
         ],
         keywords="firefox fuzz fuzzing test testing",
         license="MPL 2.0",
+        long_description=README,
+        long_description_content_type="text/markdown",
         maintainer="Mozilla Fuzzing Team",
         maintainer_email="fuzzing@mozilla.com",
         name="prefpicker",
         packages=["prefpicker"],
         url="https://github.com/MozillaSecurity/prefpicker",
-        version="1.0.1")
+        version="1.0.2")
