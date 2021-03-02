edc8273ef49dedb3bf1521d53774a1ecfbe97c52
Jesse Schwartzentruber <truber@mozilla.com>
2021-03-02 16:41:42 -0500

    Use tox & pre-commit for tests and lint.
    - yesqa only runs in pre-commit
    - tox lint versions are unpinned to catch changes in linters for manual test runs and CI
    - if linter versions require code changes, `pre-commit autoupdate`
      should be run to update pinned versions for pre-commit

--- setup.py
+++ setup.py
@@ -1,43 +1,17 @@
 #!/usr/bin/env python
 # coding=utf-8
+#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """setuptools install script"""
-import pathlib
+
+import site
+
 from setuptools import setup
 
-if __name__ == "__main__":
-    README = (pathlib.Path(__file__).parent / "README.md").read_text()
+site.ENABLE_USER_SITE = True
 
-    setup(
-        author="Tyson Smith",
-        author_email="twsmith@mozilla.com",
-        classifiers=[
-            'Topic :: Software Development :: Testing',
-            'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-            'Programming Language :: Python :: 3',
-        ],
-        description="PrefPicker - Manage & generate prefs.js files",
-        entry_points={
-            "console_scripts": ["prefpicker = prefpicker.main:main"]
-        },
-        extra_requires=[
-            "pytest",
-            "pytest-cov",
-            "pytest-pylint"
-        ],
-        include_package_data=True,
-        install_requires=[
-            "pyyaml"
-        ],
-        keywords="firefox fuzz fuzzing test testing",
-        license="MPL 2.0",
-        long_description=README,
-        long_description_content_type="text/markdown",
-        maintainer="Mozilla Fuzzing Team",
-        maintainer_email="fuzzing@mozilla.com",
-        name="prefpicker",
-        packages=["prefpicker"],
-        url="https://github.com/MozillaSecurity/prefpicker",
-        version="1.0.16")
+
+if __name__ == "__main__":
+    setup()
